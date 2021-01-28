b8bc441fea9f9a38f181363ec2eccd693cf31ec9
keskin061 <45969842+keskin061@users.noreply.github.com>
2021-01-28 15:39:23 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+class Dog():
+    def __init__(self, name):
+        self.name = name
+
+    def sit(self):
+        print(self.name + "is sitting")
+
+
+class SARDog(Dog):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def search(self):
+        print(self.name + "is searching")
+
+
+snoop = Dog("Snoop")
+sniffy = SARDog("Sniffy")
+
+print(f"{snoop.name} & {sniffy.name} are both great dogs")
+sniffy.sit()
+sniffy.search()
